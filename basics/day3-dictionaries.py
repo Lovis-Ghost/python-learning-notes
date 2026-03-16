4e7e64389c79b286c5932dcd2ae9d66684dc36b1
Lovis-Ghost <chengenxu09@gmail.com>
2026-03-16 14:51:51 +0800

    Create day3-dictionaries.py

---
+++ basics/day3-dictionaries.py
@@ -0,0 +1,15 @@
+# Day 3 - Python Dictionaries
+
+student = {
+    "name": "Lovis",
+    "age": 24,
+    "major": "Artificial Intelligence"
+}
+
+print("Student name:", student["name"])
+print("Student age:", student["age"])
+
+# add new data
+student["university"] = "UKM"
+
+print("Updated dictionary:", student)
